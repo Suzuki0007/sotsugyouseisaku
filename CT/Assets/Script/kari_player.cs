86d50ae6ae045904c296570595834ab2e575fb6a
narita <125cr0003@tokyo.amg.ac.jp>
2026-09-17 17:29:23 +0900

    画像配置して動かしてみた

--- CT/Assets/Script/kari_player.cs
+++ CT/Assets/Script/kari_player.cs
@@ -5,12 +5,12 @@ public class kari_player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(1, 0, 0);
     }
 }
